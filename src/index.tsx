492d863ee4308a8e5644df4e30578fcb608c9803
hrhr49 <hiro49410@gmail.com>
2021-10-23 21:35:21 +0900

    PWA cache support.

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 import {App} from './components/App';
+import {isDev} from './env';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -17,7 +18,11 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+if (isDev) {
+  serviceWorkerRegistration.unregister();
+} else {
+  serviceWorkerRegistration.register();
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
